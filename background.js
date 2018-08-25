d40f955063fc1dc9f16890dd0148880b7facdf7d
brycebeagle <bryce.beagle@gmail.com>
2018-08-25 12:31:32 -0700

    Rename the keyboard shortcut and make it not break on reinstall

--- background.js
+++ background.js
@@ -1,6 +1,13 @@
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.set({color: '#3aa757'}, function () {
+        console.log("The color is green.");
+    });
+});
+
 chrome.commands.onCommand.addListener(function (command) {
     chrome.storage.sync.get('color', function (data) {
         color = data.color;
+        console.log('Color:', color);
         chrome.tabs.executeScript(
             {code: 'document.body.style.backgroundColor = "' + color + '";'});
     });
