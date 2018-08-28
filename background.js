801070765f8593dc26188862fc882b4b21433f85
brycebeagle <bryce.beagle@gmail.com>
2018-08-27 23:25:55 -0700

    Initial database functionality

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 "use strict";
 
-chrome.runtime.onInstalled.addListener(function() {
-    get_all_emojis();
+chrome.runtime.onInstalled.addListener(function () {
+    init_database(get_all_emojis)
 });
 
 chrome.commands.onCommand.addListener(function (command) {
@@ -10,24 +10,38 @@ chrome.commands.onCommand.addListener(function (command) {
     }
 });
 
+function init_database(callback) {
+    // No reason to keep old version around
+    indexedDB.deleteDatabase("emoji_db");
+
+    // Create a new database to hold emojis
+    let request = indexedDB.open('emoji_db', 1);
+    request.onupgradeneeded = function (event) {
+
+        let database = event.target.result;
+        database.createObjectStore("emojis");
+
+        callback(database);
+    };
+}
+
 function send_signal() {
     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {});
     });
 }
 
-function get_all_emojis() {
+function get_all_emojis(database) {
     let request = new XMLHttpRequest();
     request.addEventListener("load", function () {
-        save_all_emojis(request.responseText);
+        save_all_emojis(request.responseText, database);
     });
     request.open("GET", "https://unicode.org/Public/emoji/11.0/emoji-test.txt");
     request.send();
 }
 
-function save_all_emojis(response_text) {
+function save_all_emojis(response_text, database) {
     let lines = response_text.split(/[\n\r]+/g);
-    let i = 0;
     for (let line of lines) {
         let emoji_line_regex = /^(.+);\s+fully-qualified\s+#\s+(\S+)\s+(.*)?$/gmu;
         let match = emoji_line_regex.exec(line);
@@ -35,7 +49,6 @@ function save_all_emojis(response_text) {
             let code_points = match[1].trim().split();
             let emoji = match[2];
             let name = match[3];
-            console.log(i++, "Code Points:", code_points, "| Emoji:", emoji, "| Name: ", name);
         }
     }
 }
