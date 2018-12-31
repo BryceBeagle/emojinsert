0c9440bcdbdc181838d5c052c94ba2fd45ed83a9
brycebeagle <bryce.beagle@gmail.com>
2018-12-30 18:55:09 -0800

    Start using custom components again. This was a nightmare to get working

--- background/background.js
+++ background/background.js
@@ -1,13 +1,40 @@
 "use strict";
 
+let loaded = {};
+
 chrome.runtime.onInstalled.addListener(function () {
     init_database();
 });
 
 chrome.commands.onCommand.addListener(function (command) {
     if (command === "show_emoji_selector") {
-        send_message({"id": "open_emoji_selector"});
-        return true;
+
+        // Get current tab ID
+        chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+            let tab = tabs[0];
+
+            // Only load once per page
+            if (!loaded[tab.id]) {
+                chrome.tabs.insertCSS(tab.id, {file: 'emoji-selector/emoji-selector.css'});
+                chrome.tabs.executeScript(tab.id, {file: 'node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle.js'});
+                chrome.tabs.executeScript(tab.id, {file: 'emoji-selector/emoji-selector-util.js'});
+                chrome.tabs.executeScript(tab.id, {file: 'emoji-selector/emoji-selector-elements.js'});
+                chrome.tabs.executeScript(tab.id, {file: 'emoji-selector/emoji-selector.js'});
+
+                loaded[tab.id] = true;
+
+                send_message({id: "open_emoji_selector"});
+            }
+
+            // If this current tab changes at all (i.e. page change) we need to reload scripts
+            chrome.tabs.onUpdated.addListener(function (tabId) {
+                if (tabId === tab.id) {
+                    console.log("Updated " + tab.id);
+                    loaded[tabId] = false;
+                }
+            });
+            return true;
+        });
     }
 });
 
