d1ab3001c1310a06e46f9f0242d97f613928e101
brycebeagle <bryce.beagle@gmail.com>
2019-02-06 23:00:03 -0800

    Move emoji_search to util file

--- emoji-selector/emoji-selector-util.js
+++ emoji-selector/emoji-selector-util.js
@@ -6,3 +6,14 @@ function emoji_search(query, callback) {
         callback(emojis)
     });
 }
+
+/* Get the active textbox on the window, ie. the one with the blinking cursor the user is typing in */
+function get_active_textbox() {
+    let activeElement = document.activeElement;
+
+    // Is there an active element, and if so, is it of type `text`? Other types of active elements could be checkboxes
+    // or radio buttons
+    if (activeElement && activeElement.contentEditable) {
+        return activeElement;
+    }
+}
--- emoji-selector/emoji-selector.js
+++ emoji-selector/emoji-selector.js
@@ -3,17 +3,6 @@
 // Global storage of the emoji selector to avoid creating it twice on the same page
 let emojinsert = null;
 
-/* Get the active textbox on the window, ie. the one with the blinking cursor the user is typing in */
-function get_active_textbox() {
-    let activeElement = document.activeElement;
-
-    // Is there an active element, and if so, is it of type `text`? Other types of active elements could be checkboxes
-    // or radio buttons
-    if (activeElement && activeElement.contentEditable) {
-        return activeElement;
-    }
-}
-
 /* Add a listener for the `open_emoji_selector` message. This comes from the background.js page */
 chrome.runtime.onMessage.addListener(
     function (message, _sender, sendResponse) {
